e71a2932c8dd6127e56d84f9b5bcd53e1c6e0fae
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-23 15:50:59 +0300

    When the sign-up link is selected the sign-in component emits an event in which the sign-in component gets deactivated and the sign-up component activated. I used event emitters. A service just doing that change isnt needed.

--- assets/app/authentication/authentication.component.ts
+++ assets/app/authentication/authentication.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-authentication',
     templateUrl: './authentication.component.html'
 })
 export class AuthenticationComponent {
+    signUpSelected:boolean = false;
+    signInSelected:boolean = true;
 
+    onFormChange(event) {
+        this.signUpSelected = !this.signUpSelected;
+        this.signInSelected = !this.signInSelected;
+    }
 }
--- assets/app/authentication/sign-in/sign-in.component.ts
+++ assets/app/authentication/sign-in/sign-in.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
     selector: 'app-sign-in',
     templateUrl: './sign-in.component.html'
 })
 export class SignInComponent {
+    signUpSelected: boolean = false;
+    @Output() changeForm = new EventEmitter<boolean>();
+
+    onSignUpSelected() {
+        this.signUpSelected = true;
+        this.changeForm.emit(this.signUpSelected);
+        console.log("Change to sign up");
+
+    }
 
 }
