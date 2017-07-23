34c57a753716f7ee465e95bebb2e44917f332d23
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-23 18:53:18 +0300

    I used bootstrap for better design of the messages. Also i used the formsGroup module to validate that there is correct input into out forms in order for the user to press the submit button

--- assets/app/authentication/sign-up/sign-up.component.ts
+++ assets/app/authentication/sign-up/sign-up.component.ts
@@ -1,14 +1,28 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {NgForm, FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-sign-up',
-    templateUrl: './sign-up.component.html'
+    templateUrl: './sign-up.component.html',
+    styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
+    signUpForm: FormGroup;
     @Output() changeToSignIn = new EventEmitter<boolean>();
 
     onSignInSelected() {
         this.changeToSignIn.emit(true);
-        console.log('change to sign in')
+        console.log('change to sign in');
+    }
+
+    ngOnInit() {
+        this.signUpForm = new FormGroup({
+            email: new FormControl(null, Validators.required),
+            password: new FormControl(null, Validators.required)
+        });
+    }
+
+    onSubmit(form: NgForm) {
+        console.log(form);
     }
 }
