f4b1b178aa18ae4d3f2a902028e626b99d2665d5
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-23 13:57:40 +0300

    Created the authentication, sign-in and sign-up components and general structure . The components are also  declared in the app.module.ts file.

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import { AuthenticationComponent} from "./authentication/authentication.component";
+import {SignInComponent} from "./authentication/sign-in/sign-in.component";
+import {SignUpComponent} from "./authentication/sign-up/sign-up.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AuthenticationComponent,
+        SignInComponent,
+        SignUpComponent
     ],
     imports: [BrowserModule],
     bootstrap: [AppComponent]
---
+++ assets/app/authentication/sign-up/sign-up.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-sign-up',
+    templateUrl: './app.component.html'
+})
+export class SignUpComponent {
+
+}
