d08904c74bfbbc47d4834274a7bccec312dc09a8
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-23 13:59:49 +0300

    The commit didnt work for what i wrote in the files. Only the added could be commited and not their code...

--- assets/app/authentication/sign-up/sign-up.component.ts
+++ assets/app/authentication/sign-up/sign-up.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-sign-up',
-    templateUrl: './app.component.html'
+    templateUrl: './sign-up.component.html'
 })
 export class SignUpComponent {
 
