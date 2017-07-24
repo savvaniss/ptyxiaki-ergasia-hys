1f83c8b7e2596253f984018d7e1bbfbe015d979f
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-24 18:34:56 +0300

    I added a first name and last name inputs for the sign up form. Also now the email input should look like an email (i used a regex) in order for the validator to accept it. Also the form is reset upon pressing the submit button

--- assets/app/authentication/sign-up/sign-up.component.ts
+++ assets/app/authentication/sign-up/sign-up.component.ts
@@ -8,21 +8,28 @@ import {NgForm, FormGroup, FormControl, Validators} from "@angular/forms";
 })
 export class SignUpComponent implements OnInit{
     signUpForm: FormGroup;
-    @Output() changeToSignIn = new EventEmitter<boolean>();
+ /*   @Output() changeToSignIn = new EventEmitter<boolean>();
 
     onSignInSelected() {
         this.changeToSignIn.emit(true);
         console.log('change to sign in');
-    }
+    }*/
 
     ngOnInit() {
         this.signUpForm = new FormGroup({
-            email: new FormControl(null, Validators.required),
+            firstname: new FormControl(null, Validators.required),
+            lastname: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+            ]),
             password: new FormControl(null, Validators.required)
+
         });
     }
 
     onSubmit(form: NgForm) {
-        console.log(form);
+        console.log(form.form.value);
+        this.signUpForm.reset();
     }
 }
