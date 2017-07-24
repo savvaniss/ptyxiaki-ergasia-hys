13f6f41d6fa1cc860a25c33ed23ba3e24a3160d1
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-24 17:44:55 +0300

    Changed the names of the routing directories

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -4,9 +4,9 @@ import { SignUpComponent } from "./authentication/sign-up/sign-up.component"
 import { SignInComponent } from "./authentication/sign-in/sign-in.component"
 
 const APP_ROUTES: Routes = [
-    {path: '', redirectTo: '/signIn', pathMatch: 'full' },
-    {path: 'signIn', component: SignInComponent },
-    {path: 'signUp', component: SignUpComponent }
+    {path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+    {path: 'sign-in', component: SignInComponent },
+    {path: 'sign-up', component: SignUpComponent }
 
 ];
 
